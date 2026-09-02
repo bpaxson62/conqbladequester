7be76abca64bd038b2ce83a9c533211efa5cbbb3
Greg <gjpaxs@gmail.com>
2026-09-02 19:13:26 -0400

    I am adding season 17 stage 1?

---
+++ data/unlocks/season17/CaradocsCavalry.js
@@ -0,0 +1,19 @@
+module.exports = {
+  name: "Caradoc's Cavalry",
+  id: 'caradoc-s-cavalry',
+  rewardType: 'unit',
+  tier: 1,
+  season: 17,
+  prerequisites: [],
+  requiredPerStage: 6,
+  unitChallenges: [
+    { key: 0, text: 'Destroy a siege engine or acquire 40 capture points in 6 Field or Siege Battles of any type.', stage: 1 },
+    { key: 1, text: 'Get a Troop Kill Score of 400 or higher with any unit in 6 Field or Siege Battles of any type.', stage: 1 },
+    { key: 2, text: 'Get a Troop Kill Score of 1000 or higher in 4 Field or Siege Battles of any type.', stage: 1 },
+    { key: 3, text: 'Defeat 1,000 enemy units in PVE battles.', stage: 1 },
+    { key: 4, text: 'Get the "Hat-Trick" or "A Deadly Century" badge in 5 Field or Siege Battles of any type.', stage: 1 },
+    { key: 5, text: 'Get 30 kills or assists in Free Battles, or get 8 kills or assists in 6 Field or Siege Battles of any type.', stage: 1 },
+    { key: 6, text: 'Complete 10 Daily Quests.', stage: 1 },
+    { key: 7, text: 'Complete 10 Fief Quests.', stage: 1 }
+  ]
+}
